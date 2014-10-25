d2b4bafe23eeae0f6e2c1890da68b38492d8a006
tdopires <tdopires@gmail.com>
2014-10-26 00:02:24 +0200

    Organizing resources

--- gather_ontology_owl_files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-cp schema-and-top-artists.owl.part1 schema-and-top-artists.owl && cat schema-and-top-artists.owl.part2 >> schema-and-top-artists.owl && cat schema-and-top-artists.owl.part3 >> schema-and-top-artists.owl
-
